0ef8c322edf5c3c8b6241933eb10b01f989adc4c
Omer Sharon <your-email@example.com>
2025-02-11 15:08:21 +0700

    config.py

---
+++ Uploader/Youtube/AutoUpload - Youtube/Code/config.py
@@ -0,0 +1,3 @@
+PLATFORM_URL = "https://youtube.com/studio"
+CHANNELS_ROOT_DIR = "C:\\Presence\\Presence0.1\\Channels"
+RESOURCES_DIR = "C:\\Presence\\Presence0.1\\Uploader\\Resources"
---
+++ Uploader/Youtube/main/config.py
@@ -0,0 +1,2 @@
+ROOT_DIR = "C:\\Presence\\Presence0.1\\Uploader\\Youtube"
+CHANNELS_ROOT_DIR = "C:\\Presence\\Presence0.1\\Channels"
