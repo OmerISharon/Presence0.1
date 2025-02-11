2b8134c04c0e0c058cb96315d931d580acade92c
Omer Sharon <your-email@example.com>
2025-02-11 15:04:17 +0700

    God Mode Notes full batch

---
+++ Channels/God Mode Notes/Batch/God Mode Notes.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d C:\Presence\Presence0.1\Creator\GodModeNotes\Code
+python Creator_GodModeNotes.py
+
+cd /d C:\Presence\Presence0.1\Uploader\Youtube\main
+python main.py "God Mode Notes"
+
+exit
