0a15a51306e6fb30506699a162983d7b956f2b77
Omer Sharon <omersh60@gmail.com>
2025-03-04 21:04:42 +0200

    gitignore file changes

---
+++ Uploader/Tiktok/AutoUpload - Tiktok/Code/config.py
@@ -0,0 +1,2 @@
+CHANNELS_ROOT_DIR = "D:\\2025\\Projects\\Presence\\Presence0.1\\Channels"
+RESOURCES_DIR = "D:\\2025\\Projects\\Presence\\Presence0.1\\Uploader\\Resources"
---
+++ Uploader/config.py
@@ -0,0 +1,4 @@
+CHANNELS_ROOT_DIR = "D:\\2025\\Projects\\Presence\\Presence0.1\\Channels"
+LOGS_DIR = "D:\\2025\\Projects\\Presence\\Presence0.1\\Uploader\\Logs"
+UPLOADER_SCRIPT_YOUTUBE = "D:\\2025\\Projects\\Presence\\Presence0.1\\Uploader\\Youtube\\AutoUpload - Youtube\\Code\\AutoUploadYouTube.py"
+UPLOADER_SCRIPT_TIKTOK = "D:\\2025\\Projects\\Presence\\Presence0.1\\Uploader\\Tiktok\\AutoUpload - Tiktok\\Code\\AutoUploadTikTok.py"
